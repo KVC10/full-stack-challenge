e30c434c8fc765f47b5276aef8bc18c0a05e6ba2
Vianney Christ Konan <vianneychrist271@gmail.com>
2026-09-15 20:25:51 +0000

    update index.js

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -99,9 +99,10 @@ app.post("/api/persons", (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "../../part2/Phonebook/dist")));
-app.get("*", (req, res) => {
+//react router
+/* app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../part2/Phonebook/dist/index.html"));
-});
+}); */
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
